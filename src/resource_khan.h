2c965e2eb06a313e6a513825045300a2412dfae7
schilkp <schilk.philipp@gmail.com>
2024-01-26 12:25:19 +0100

    Have default conf in main header instead of seperate file.

--- src/resource_khan.h
+++ src/resource_khan.h
@@ -14,7 +14,40 @@
 #ifdef RK_USE_CUSTOM_CONF
 #include "resource_khan_conf_custom.h"
 #else
-#include "resource_khan_conf.h"
+
+#include <stdio.h>
+
+/** @brief Maximum length of node/client name  */
+#define RK_MAX_NAME_LEN 15
+
+/** @brief Maximum number of parent nodes per node or client */
+#define RK_MAX_PARENTS  4
+
+/** @brief Maximum number of children nodes and clients per node */
+#define RK_MAX_CHILDREN 4
+
+/**
+ * @brief Return value indicating a function did not complete succesfully
+ * @note Must be an 'int' value that is not '0'
+ */
+#define RK_ERR          1
+
+/** @brief Information log function */
+#define RK_LOG_INF(_fmt_, ...)                                                                                         \
+  do {                                                                                                                 \
+    printf("INF: "_fmt_                                                                                                \
+           "\n",                                                                                                       \
+           __VA_ARGS__);                                                                                               \
+  } while (0)
+
+/** @brief Error log function */
+#define RK_LOG_ERR(_fmt_, ...)                                                                                         \
+  do {                                                                                                                 \
+    printf("ERR: "_fmt_                                                                                                \
+           "\n",                                                                                                       \
+           __VA_ARGS__);                                                                                               \
+  } while (0)
+
 #endif /* RK_USE_CUSTOM_CONF */
 
 /**
